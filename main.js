8ffc05fe007a4a49a7338a0b7c8cd2bb215b8eea
Mike Batelahoko <mikebatelahoko96@gmail.com>
2024-07-14 23:33:50 +0200

    all exercise are done.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // ----------------- script 1 -----------------
+
 let a = 1;
 let b = 5;
 let temp;
@@ -10,29 +11,63 @@ b = temp; // b vaut 1 à cause de temp qui vaut 1.
 console.log("* --------- Script 1 --------- *");
 console.log("A:", a, "B:", b);
 
-// ----------------- script 2 -----------------
+// ----------------- script 2 ----------------- 
+
 const mySecondExo = () => {
-    let fName = "toto";
-    let age = 30;
-    let homme = true;
+  let fName = "toto";
+  let age = 30;
+  let homme = true;
 
-    console.log("* --------- Script 2 --------- *");
-    // 
-    console.log("Nom : %s , age : %s , homme : %s", fName, age, homme);
+  console.log("* --------- Script 2 --------- *");
 
-    if(homme === true) {
-        console.log("Sexe : homme" );
-    } else {
-        console.log("Sexe : Femme");
-    }
-}
-mySecondExo(); 
+  console.log("Nom : %s , age : %s , homme : %s", fName, age, homme);
+
+  if (homme === true) {
+    console.log("Sexe : homme"); 
+  } else {
+    console.log("Sexe : Femme");
+  }
+};
+mySecondExo(); // call the function.
 
 // ----------------- script 3 -----------------
-// le module readline permet la lecture du flux d'entrée ligne par ligne.
-const readline = require('node:readline');
 
-const rl = readline.createInterface({ // la méthode de l'interface prend deux arguments. 
-    input: process.stdin, // le premier sera pour l'entrée standard 
-    input: process.stdout, // et le second pour la lecture de la sortie. 
+// le module readline permet la lecture du flux d'entrée ligne par ligne.
+const readline = require("readline");
+
+const PUISSANCE = 2;
+
+// créer une interface readline pour script 3 et s'assurer qu'elle est fermée avant de passer à autre chose.
+const rl1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl1.question("Quelle est la puissance voulue ? ", (result) => {
+  console.log(
+    `${PUISSANCE} à la puissance ${result} = `,
+    Math.pow(PUISSANCE, result)
+  );
+  rl1.close();
+
+  // Script 4 commence ici, après que rl1 ait été fermée.
+  const rl2 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl2.question("Quel est votre nom ? ", (answer1) => {
+    rl2.question("Quel est votre prénom ? ", (answer2) => {
+      rl2.question("Quel est votre age ? ", (answer3) => {
+        console.log("---------------------------");
+        console.log("---------BIENVENUE---------");
+        console.log("---------------------------");
+
+        console.log(`votre nom est : ${answer1}`);
+        console.log(`Votre prénom est : ${answer2}`);
+        console.log(`Vous avez ${answer3} ans`);
+        rl2.close();
+      });
+    });
+  });
 });
